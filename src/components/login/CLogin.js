0e0da8a99a09d4e97426ac799adba9450226640a
sushil <sushil.sth.47@gmail.com>
2020-02-13 21:49:29 +0545

    ui updated

--- src/components/login/CLogin.js
+++ src/components/login/CLogin.js
@@ -4,15 +4,19 @@ import "./login.css";
 import { connect } from "react-redux";
 
 class CLogin extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    }
     this.submitLogin = this.submitLogin.bind(this);
   }
 
-  componentWillMount() {
-    if (this.props.isLoggedIn || localStorage.getItem("token")) {
-      this.props.history.push("/dashboard");
+  componentWillMount(){
+    if(this.props.isLoggedIn && localStorage.getItem("token")){
+      window.location.href = "/dashboard"
     }
+
   }
 
   submitLogin(event) {
@@ -25,38 +29,53 @@ class CLogin extends React.Component {
     const password = event.target.form.elements.password.value;
 
     if (username === "" || password === "") {
-      alert(" please fill both");
+      this.setState({
+        message: "Please fill username and password."
+      })
       return false;
     }
 
-    fetch("http://localhost:3005/api/gs/login", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password
+    try {
+
+      fetch("http://localhost:3005/api/gs/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
       })
-    })
-      .then(results => results.json())
-      .then(data => {
-        if (data.token && data.message === "authenticated") {
-          localStorage.setItem("token", data.token);
-          this.props.toggleLoggedStatus();
-          this.props.history.push("/dashboard");
-        } else {
-          alert(data.message);
-        }
-      });
+        .then(results => results.json())
+        .then(data => {
+          if (data.token && data.message === "authenticated") {
+            localStorage.setItem("token", data.token);
+            this.props.toggleLoggedStatus();
+            this.props.history.push("/dashboard");
+          } else {
+            this.setState({
+              message: data.message
+            })
+          }
+        });
+
+    } catch (error) {
+
+      alert(error)
+
+    }
+
   }
 
   render() {
     return (
       <div>
-        <h1>{this.props.isLoggedIn ? "Logged IN" : "Not Logged IN"}</h1>
-        <VLogin submitLogin={this.submitLogin} />
+        {/* ` <h1>{this.props.isLoggedIn ? "Logged IN" : "Not Logged IN"}</h1>` */}
+        <VLogin
+          message={this.state.message}
+          submitLogin={this.submitLogin} />
       </div>
     );
   }
--- src/components/login/VLogin.js
+++ src/components/login/VLogin.js
@@ -1,29 +1,41 @@
 import React from "react";
-import { Container, Row, Form, Button, Image } from "react-bootstrap";
-import brandLogo from "../../../src/assests/images/brand.png";
+import { Container, Row, Form, Button } from "react-bootstrap";
+/* import brandLogo from "../../../src/assests/images/gym.png"; */
 
 function VLogin(params) {
   return (
-    <div id="login-section">
-      <Container>
-        <Row>
-          <Image src={brandLogo}></Image>
-        </Row>
-        <Row>
-          <Form>
-            <Form.Group controlId="username">
-              <Form.Control type="text" placeholder="Username"></Form.Control>
-            </Form.Group>
 
-            <Form.Group controlId="password">
-              <Form.Control type="password" placeholder="Password"></Form.Control>
-            </Form.Group>
+    <Container>
 
-            <Button variant="primary" type="submit" onClick={event => params.submitLogin(event)}>Login</Button>
-          </Form>
-        </Row>
-      </Container>
-    </div>
+      <div className="card card-login mx-auto text-center bg-dark">
+
+        <div className="card-header mx-auto bg-dark">
+          <span>
+            <img src="https://amar.vote/assets/img/amarVotebd.png" className="w-75" alt="Logo" />
+          </span><br />
+          <span className="logo_title mt-5"> Login Dashboard </span>
+          <br />
+          <span id="message">{params.message}</span>
+        </div>
+
+        <div className="card-body">
+          <Row>
+            <Form>
+              <Form.Group controlId="username">
+                <Form.Control type="text" placeholder="Username"></Form.Control>
+              </Form.Group>
+
+              <Form.Group controlId="password">
+                <Form.Control type="password" placeholder="Password"></Form.Control>
+              </Form.Group>
+
+              <Button variant="primary" type="submit" onClick={event => params.submitLogin(event)}>Login</Button>
+            </Form>
+          </Row>
+        </div>
+      </div>
+    </Container>
+
   );
 }
 export default VLogin;
